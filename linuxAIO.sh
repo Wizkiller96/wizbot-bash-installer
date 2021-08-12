3f96d19a8ee494128ccd717dcf5eeb7d446e1caa
Kwoth <master.kwoth@gmail.com>
2021-08-12 14:38:48 +0200

    Fixed menu url in linuxAIO.sh

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Welcome to NadekoBot. Downloading the latest installer..."
 root=$(pwd)
-wget -N https://gitlab.com/Kwoth/nadeko-bash-installer/raw/1.9/n-menu.sh
+wget -N https://gitlab.com/Kwoth/nadeko-bash-installer/-/raw/master/n-menu.sh
 
 bash n-menu.sh
 cd "$root"
